1d0a3c434a78d045cf0ba4b97bf54f41727a86f3
Nadya Voynich <nadya.voynich@gmail.com>
2024-10-20 14:42:26 +0200

    chore: Resolve dependencies conflict

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import requests.exceptions
+from json.decoder import JSONDecodeError
+requests.exceptions.JSONDecodeError = JSONDecodeError
+
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 from app_store_scraper import AppStore
@@ -11,11 +15,11 @@ client = gspread.authorize(credentials)
 sheet = client.open('AppStore Reviews').sheet1
 
 # Fetch App Store reviews for an app
-app = AppStore(app_name="ankimobile-flashcards", app_id="373493387")
+app = AppStore(country="us", app_name="ankimobile-flashcards", app_id="373493387")
 app.review(how_many=10000)  # Number of reviews to scrape
 
 # Extract stars, text, and date for each review
-reviews_data = [(review['rating'], review['review'], review['date']) for review in app.reviews]
+reviews_data = [(review['rating'], review['review'], review['date'].isoformat()) for review in app.reviews]
 
 # Batch write to Google Sheets (reducing the number of API write requests)
 sheet.append_rows(reviews_data)
